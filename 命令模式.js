43adf1b361ee43050c4fb698f2d9297859472c0d
yang123guo <289656703@qq.com>
2017-06-10 23:22:33 +0800

    啥都不会写

--- 命令模式.js
+++ 命令模式.js
@@ -1,3 +1,12 @@
+// 命令模式分为：简单命令模式 复杂命令模式 闭包封装命令模式
+var CommandInterface = new BH.Interface('CommandInterface', ['execute'])
+
+// 命令模式的主要概念
+
+
+
+
+
 // 闭包命令模式
 function makeStart() {
     return function() {
